4a2028a2315bc24dfb4de63238be3a632686f7ea
Maykal Tenev <michaeltenev@gmail.com>
2024-01-28 19:54:02 +0100

    nav manu and dropdown menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+// Header.jsx
+
+import React, { useState } from "react";
 import "./Header.css";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header>
       <nav>
         <div className="logo">
           <img src="logo.png" alt="Logo" />
         </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#">Add Word</a>
-          </li>
-          <li>
-            <a href="#">Random</a>
-          </li>
-          <li>
-            <a href="#">All</a>
-          </li>
-        </ul>
+        <div className={`menu ${showMenu ? "active" : ""}`}>
+          <div className="nav-links">
+            <ul>
+              <li>
+                <a href="#">Add Word</a>
+              </li>
+              <li>
+                <a href="#">Random</a>
+              </li>
+              <li>
+                <a href="#">All</a>
+              </li>
+            </ul>
+          </div>
+          <div className="hamburger" onClick={toggleMenu}>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+          </div>
+          {showMenu && (
+            <div className="dropdown">
+              <button>Add Word</button>
+              <button>Random</button>
+              <button>All</button>
+            </div>
+          )}
+        </div>
       </nav>
     </header>
   );
